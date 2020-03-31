9f450fc8b79a15d798e518ac9e16d4d32351e89c
nickp60 <nickp60@gmail.com>
2020-03-31 19:58:41 +0100

    fix paths, update training set

--- docs/analysis/training/extract_amplicons.sh
+++ docs/analysis/training/extract_amplicons.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -e
+#$ -cwd
+#$ -j yes
+#$ -N extract
+#$ -pe mpi 48
 
 # this run the extraction and alignment of the Clermont PCR amplicons.
 # its pretty slow; the bottleneck is the extractRegion script; i think its O(n),
@@ -11,18 +14,19 @@ DATE=`date +%Y-%m-%d`
 # make the output directory
 OUTDIR="./${DATE}_clermont_region_extraction/"
 mkdir $OUTDIR
+set -e
 
 # for each of the amplicons in the Clermnt scheme
-for GENEF in refs/*.fasta
+for GENEF in refs/trpBA.fasta  refs/TspE4.C2.fasta  refs/yjaA.fasta
 do
   # get the name of the gene from the file name
   BN=$(basename $GENEF)
   REG=${BN%.fasta}
   echo $REG
   # find the coords of the ortholog
-  ~/GitHub/open_utils/orthoML/simpleOrtho.py $GENEF ./combined/ -n -v 1 -o $OUTDIR/${REG}_out -t 4
+  simpleOrtho.py $GENEF ./combined/ -n -v 1 -o $OUTDIR/${REG}_out -t 4
   # get the sequence of the othologs
-  ~/GitHub/open_utils/extractRegion/extractRegion.py -l ./$OUTDIR/${REG}_out/simpleOrtho_regions.txt ./combined.fasta > $OUTDIR/${REG}_regions.fasta
+  ~/GitHub/open_utils/extractRegion/extractRegion.py -l ./$OUTDIR/${REG}_out/simpleOrtho_regions.txt -f ./combined.fasta > $OUTDIR/${REG}_regions.fasta
   # align all those
   mafft --thread 4 ./$OUTDIR/${REG}_regions.fasta > ./$OUTDIR/${REG}_regions_aligned.fasta
   # get rid of big temp files
